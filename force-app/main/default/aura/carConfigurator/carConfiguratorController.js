a52df684424f620f57d69f6a668e3362b666c471
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-21 16:41:00 -0500

    more styles layout fixes, metric conversion

--- force-app/main/default/aura/carConfigurator/carConfiguratorController.js
+++ force-app/main/default/aura/carConfigurator/carConfiguratorController.js
@@ -1,5 +1,5 @@
 ({
-    doInit : function(component) {
+    doInit : function(component, event, helper) {
         component.set('v.selectedVehicle', null);
         component.set('v.data', [
             {
@@ -9,7 +9,9 @@
                         label: 'XC90',
                         description: 'Experience uncluttered design with cutting-edge innovation creating a calming Scandinavian sanctuary.',
                         mpg: 29,
+                        L100km: helper.mpgToL100km(29),
                         hp: '250-400',
+                        kW: helper.hpTokW(250, 400),
                         img: 'https://cas.volvocars.com/image/vbsNext-v4/exterior/MY17_1617/256/10/52/61400/R118/_/TP03/_/_/_/_/SR03/_/_/JB07/T211/SideModelpage.jpg?bg=ffffff&market=us&fallback=1&w=375',
                         options: [{
                             engine: [
@@ -17,28 +19,32 @@
                                     name: 'T5 FWD',
                                     hp: 250,
                                     torque: '258',
-                                    mpg: '29',
+                                    mpg: 29,
+                                    L100km: helper.mpgToL100km(29),
                                     price: 46900
                                 },
                                 {
                                     name: 'T5 AWD',
                                     hp: 250,
                                     torque: '258',
-                                    mpg: '28',
+                                    mpg: 28,
+                                    L100km: helper.mpgToL100km(28),
                                     price: 49100
                                 },
                                 {
                                     name: 'T6 AWD',
                                     hp: 316,
                                     torque: '295',
-                                    mpg: '27',
+                                    mpg: 27,
+                                    L100km: helper.mpgToL100km(27),
                                     price: 54350
                                 },
                                 {
                                     name: 'T8 eAWD Plug-in Hybrid',
                                     hp: 400,
                                     torque: '472',
-                                    mpg: '27',
+                                    mpg: 27,
+                                    L100km: helper.mpgToL100km(27),
                                     price: 64950
                                 }
                             ]
@@ -48,14 +54,18 @@
                         label: 'XC60',
                         description: 'Dynamic, refined and intuitive – the Swedish SUV has evolved.',
                         mpg: 28,
+                        L100km: helper.mpgToL100km(28),
                         hp: '250-400',
+                        kW: helper.hpTokW(250, 400),
                         img: 'https://cas.volvocars.com/image/vbsNext-v4/exterior/MY18_1717/246/10/42/61400/R11B/_/TP02/_/_/_/_/SR03/_/_/JB09/T211/SideModelpage.jpg?bg=ffffff&market=us&fallback=1&w=375'
                     },
                     {
                         label: 'XC40',
                         description: 'Strong, contemporary style with a confident urban look, available in contrasting combinations.',
                         mpg: 33,
+                        L100km: helper.mpgToL100km(33),
                         hp: '187-248',
+                        kW: helper.hpTokW(187, 248),
                         img: 'https://cas.volvocars.com/image/vbsNext-v4/exterior/MY18_1746/536/AC/42/01900/R137/_/TP02/_/_/_/_/SR06/_/_/JB09/T210/SideModelpage.jpg?bg=ffffff&market=us&fallback=1&w=375'
                     }
                 ]
@@ -71,14 +81,18 @@
                         label: 'S90',
                         description: 'Experience modern Scandinavian luxury. A luxurious sedan made by Sweden.',
                         mpg: 34,
+                        L100km: helper.mpgToL100km(34),
                         hp: '250-400',
+                        kW: helper.hpTokW(250, 400),
                         img: 'https://cas.volvocars.com/image/vbsNext-v4/exterior/MY17_1646/238/98/42/01900/R104/_/_/_/_/_/_/SR03/_/_/JB07/T211/SideModelpage.jpg?bg=ffffff&market=us&fallback=1&w=375'
                     },
                     {
                         label: 'S60',
                         description: 'Three impressive powertrains and all - around innovative and stylish features, there’s an S60 to fit your lifestyle.',
                         mpg: 36,
+                        L100km: helper.mpgToL100km(36),
                         hp: '240-362',
+                        kW: helper.hpTokW(240, 362),
                         img: 'https://cas.volvocars.com/image/vbsNext-v3/exterior/MY14_1320/134/26/36/61400/R14E/_/_/_/_/_/_/_/_/_/SideModelpage.jpg?bg=ffffff&market=us&fallback=1&w=375'
                     }
                 ]
--- force-app/main/default/aura/carConfigurator/carConfiguratorHelper.js
+++ force-app/main/default/aura/carConfigurator/carConfiguratorHelper.js
@@ -1,5 +1,11 @@
 ({
-    helperMethod : function() {
+    hpTokW : function(low, high) {
+        const lowOut = Math.round(low * 0.745699872);
+        const highOut = Math.round(high * 0.745699872);
+        return lowOut + '-' + highOut;
+    },
 
+    mpgToL100km: function (mpg) {
+        return Math.round( ( 1 / mpg ) * ( 1 / 0.264172 ) * ( 0.621371 / 1 )   * 1000 ) / 10;
     }
 })
