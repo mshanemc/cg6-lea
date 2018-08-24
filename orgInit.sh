fee7e0f1f61fb94093cb677d08eb487ab49aae9e
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-24 11:28:37 -0500

    remove boilerplate stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,9 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth -u cg6_
 sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
+sfdx shane:github:package:install -g mshanemc -r community-boilerplate
+sfdx shane:github:src:install -g mshanemc -r customer-community-base
+sfdx shane:github:package:install --githubuser mshanemc --repo carConfigurator
 sfdx shane:user:password:set -g User -l User -p DF18*cg6
 sfdx force:user:permset:assign -n VehiclesPerms
-sfdx force:source:push
 sfdx force:apex:execute -f scripts/init.cls
 sfdx force:org:open -p lightning/setup/SetupNetworks/home
